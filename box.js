4ab718b6ab60e8e25a233bbaaf9099944019b98d
abner0samuel <abaretabner@gmail.com>
2024-01-28 16:25:46 +0100

    initial commit

---
+++ box.js
@@ -0,0 +1,7 @@
+const fnum =document.querySelector('#fnumber');
+const snum = document.querySelector('#snumber');
+const tnum = document.querySelector('#tnumber');
+const fonum = document.querySelector('#fonumber');
+if(fnum.Value === ' '){
+    
+}
